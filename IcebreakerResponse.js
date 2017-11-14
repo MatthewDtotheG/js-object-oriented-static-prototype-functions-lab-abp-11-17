d836a7997380b90c5faad0987cced03c8b21c022
Matthew Garrett <mdgarrett1984@gmail.com>
2017-11-14 14:22:03 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,6 +2,7 @@ function IcebreakerResponse(email) {
   this.email = email;
 }
 
+/*
 Icebreaker.BatchCreate = function(emails) {
   return emails.map(function(userEmail)) {
     return new IcebreakerResponse(userEmail[0], userEmail[1]);
